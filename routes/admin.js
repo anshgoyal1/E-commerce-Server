d152c68a5c43f518f11cc50d8d66cf937aca6afe
Ansh goyal <anshgoyal749@gmail.com>
2023-05-18 11:32:44 +0530

    comii

--- routes/admin.js
+++ routes/admin.js
@@ -7,7 +7,6 @@ const adminRouter = express.Router();
 adminRouter.post("/admin/add-product", admin, async (req, res) => {
   try {
     const { name, description, images, quantity, price, category } = req.body;
-
     let product = new Product({
       name,
       description,
@@ -16,11 +15,10 @@ adminRouter.post("/admin/add-product", admin, async (req, res) => {
       price,
       category,
     });
-
     product = await product.save();
-    res.status(200).json({ product });
-  } catch (error) {
-    res.status(500).json({ error: error.message });
+    res.json(product);
+  } catch (e) {
+    res.status(500).json({ error: e.message });
   }
 });
 
